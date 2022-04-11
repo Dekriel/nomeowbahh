bb9555aedf0d6535a480cf05dcb114e7517be800
Dekriel <92822789+Dekriel@users.noreply.github.com>
2022-04-11 08:36:53 +0100

    updated and fixed code

--- main.py
+++ main.py
@@ -12,10 +12,9 @@ async def on_ready():
 
 @bot.command()
 async def start(ctx):
-    if ctx.guild.id == 921916567145570384:
-        while True:
-            await asyncio.sleep(2)
-            await ctx.send("<@749793965796098108> SCREW YOU")
+    while True:
+        await asyncio.sleep(2)
+        await ctx.send("<@749793965796098108> SCREW YOU")
 
 
 bot.run("YOUR TOKEN HERE", bot=False)
